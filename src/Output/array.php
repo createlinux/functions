284bb8c40bf4bf7baac6d8967d99ce289a4267bd
Administrator <howmao@qq.com>
2023-09-13 19:25:29 +0800

    数组切片

--- src/Output/array.php
+++ src/Output/array.php
@@ -1,8 +1,22 @@
 <?php
 
 if (!function_exists('array_fill_keys_by_values')) {
+    /**
+     * @param $array array 数组
+     * @param $key mixed 键名
+     * @return array
+     */
     function array_fill_keys_by_values($array, $key)
     {
         return array_combine(array_column($array, $key), $array);
+    }
+}
+
+if (!function_exists('array_slice_by_key')) {
+    function array_slice_by_key($array, $startKey, $endKey)
+    {
+        foreach ($array as $key => $value) {
+
+        }
     }
 }
